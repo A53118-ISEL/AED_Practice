659c523e88b8fbd88608f8857a65e8654b4b918d
A53118-ISEL <A53118@alunos.isel.pt>
2025-03-13 09:09:55 +0000

    4th

--- src/main/kotlin/Notes.kt
+++ src/main/kotlin/Notes.kt
@@ -1,12 +1,12 @@
-//Para ver time complexity, fazer uma série (aritmética ou geométrica) para o pior caso com n=infinito
+//Para ver time complexity, fazer uma série (aritmética ou geométrica) com um certo tipo de dados.
 //No caso de dados perversos, n = infinito
-
+//No caso de dados normais, n =
+//No caso de dados
 
 //Quando um algoritmo faz trocas aleatórias, não é "Estável"
 
 //Lowerbound e Upperbound
-//lowerBound, procura um indice onde pode ser colocado (ver foto)
+//lowerBound, procura um indice onde pode ser colocado (ver att.)
 
 //Analise de algoritmos
 //teste empirico, consome demasiado tempo
-//
